32dfffa9c1a2e049d8a7c970429fe72df7ff6def
laxmankc21 <laxman2018kc@gmail.com>
2026-03-09 17:19:11 +0545

    springbootdi

---
+++ src/main/java/com/laxman/myApp/MyAppApplication.java
@@ -0,0 +1,14 @@
+package com.laxman.myApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppApplication {
+
+	public static void main(String[] args) {
+		// create a container
+		SpringApplication.run(MyAppApplication.class, args);
+	}
+
+}
